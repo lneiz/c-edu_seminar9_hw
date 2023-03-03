fbd621d0f38c938044e421a35d584e06278a06e9
lneiz <yuntsevich@ymail.com>
2023-03-04 03:18:44 +0500

    66 - done

--- Program.cs
+++ Program.cs
@@ -1,14 +1,40 @@
 ﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
 //Выполнить с помощью рекурсии.
 
-string listN(int n)
-{
-    if(n == 1) return "1";
+// string listN(int n)
+// {
+//     if(n == 1) return "1";
 
-    return  n + ", " + listN(n - 1);
+//     return  n + ", " + listN(n - 1);
+// }
+
+// Console.Write("Введите N? ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(listN(n));
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int AdditOfNum(int m, int n)
+{
+    if(n < m)
+    {
+        if(m == n) return n;
+
+        return m + AdditOfNum(m - 1, n);
+    }
+    else
+    {
+        if(n == m) return m;
+
+        return n + AdditOfNum(n - 1, m);
+    }
 }
+
 
 Console.Write("Введите N? ");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите M? ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(listN(n));
+Console.Write("-> " + AdditOfNum(m,n));

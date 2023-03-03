6ef910a2f67fe7152f06e50059e03b5d242fb911
lneiz <yuntsevich@ymail.com>
2023-03-04 03:36:02 +0500

    66 - done

--- Program.cs
+++ Program.cs
@@ -15,26 +15,42 @@
 
 //Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-int AdditOfNum(int m, int n)
+// int AdditOfNum(int m, int n)
+// {
+//     if(n < m)
+//     {
+//         if(m == n) return n;
+
+//         return m + AdditOfNum(m - 1, n);
+//     }
+//     else
+//     {
+//         if(n == m) return m;
+
+//         return n + AdditOfNum(n - 1, m);
+//     }
+// }
+
+
+// Console.Write("Введите N? ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите M? ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("-> " + AdditOfNum(m,n));
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int AckermanFunc(int m, int n)
 {
-    if(n < m)
-    {
-        if(m == n) return n;
-
-        return m + AdditOfNum(m - 1, n);
-    }
-    else
-    {
-        if(n == m) return m;
-
-        return n + AdditOfNum(n - 1, m);
-    }
+    if(m == 0) return n + 1;
+    if(m > 0 && n == 0) return AckermanFunc(m - 1, 1);
+    if(m > 0 n > 0) return AckermanFunc(m - 1, AckermanFunc(m, n-1));
 }
-
 
 Console.Write("Введите N? ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите M? ");
 int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("-> " + AdditOfNum(m,n));
+Console.Write(AckermanFunc(m,n));
